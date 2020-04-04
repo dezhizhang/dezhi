583252ea03e96e3ece70e37b5a111076b896c9fd
dezhizhang <1541609448@qq.com>
2020-04-04 23:39:02 +0800

    轮播图管理

--- app/router.js
+++ app/router.js
@@ -5,6 +5,41 @@
  */
 module.exports = app => {
   const { router, controller } = app;
+
+  //登录
+  router.get('/admin/login',controller.admin.login.index);
+  //首页
+  router.get('/admin/home',controller.admin.home.index);
+  //管理员管理
+  router.get('/admin/manager',controller.admin.manager.index);
+  router.get('/admin/manager/add',controller.admin.manager.add);
+  //用户管理
+  router.get('/admin/user/add',controller.admin.user.add);
+  router.get('/admin/user',controller.admin.user.index);
+  
+    //分类管理
+  router.get('/admin/classify',controller.admin.classify.index);
+    router.get('/admin/classify/add',controller.admin.classify.add);
+  
+    //图片管理
+    router.get('/admin/picture',controller.admin.picture.index);
+    router.get('/admin/picture/add',controller.admin.picture.add);
+  
+    //反馈管理
+    router.get('/admin/feedback',controller.admin.feedback.index);
+  
+  
+    //活动管理
+    router.get('/admin/activity',controller.admin.activity.index);
+    router.get('/admin/activity/add',controller.admin.activity.add);
+  
+    //赞助管理
+    router.get('/admin/sponsor',controller.admin.sponsor.index);
+    router.get('/admin/sponsor/add',controller.admin.sponsor.add);
+
+
+
+
   //首页
   router.get('/', controller.default.home.index);
   router.get('/index',controller.default.home.index);
