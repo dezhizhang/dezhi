a1cd5502eb003440c7b156a3ba5da16a84eb2730
dezhizhang <1541609448@qq.com>
2020-04-05 12:29:33 +0800

    修改

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -5,6 +5,8 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
     async index() {
         let focus = await this.ctx.model.Focus.find();
+        console.log(focus);
+        
         await this.ctx.render("/default/index",{
             focus,
         })
