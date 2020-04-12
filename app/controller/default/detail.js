4d7e3a4e7703f16a448ccfad0ee589623fb10a9a
dezhizhang <1541609448@qq.com>
2020-04-12 18:24:05 +0800

    修改报错

--- app/controller/default/detail.js
+++ app/controller/default/detail.js
@@ -5,12 +5,11 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     let { article_id,name } = this.ctx.query;
-    let result = await this.ctx.model.Detail.find({'article_id':article_id});
-    console.log(result);
-
+    let list = await this.ctx.model.Detail.find({'article_id':article_id});
     await this.ctx.render('/default/detail',{
-      result:result[0],
-      title:name
+      content:list[0].content,
+      title:name,
+      list:list
     })
   }
 }
