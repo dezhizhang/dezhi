1dd4b7f61a9864bd385635acace84d2d5e784629
zhangdezhi <1541609448@qq.com>
2020-12-30 19:20:13 +0800

    文章列表

--- app/controller/admin/article.js
+++ app/controller/admin/article.js
@@ -21,25 +21,13 @@ class ArticleController extends BaseController {
         await this.ctx.render('/admin/article/add');
     }
     async doAdd() {
-        let parts = this.ctx.multipart({ autoFields: true });
-        let files = {};               
-        let stream;
-        while ((stream = await parts()) != null) {
-            if (!stream.filename) {          
-                break;
-            }       
-            let fieldname = stream.fieldname;  //file表单的名字
-            //上传图片的目录
-            let dir=await this.service.tools.getUploadFile(stream.filename);
-            let target = dir.uploadDir;
-            let writeStream = fs.createWriteStream(target);
-            await pump(stream, writeStream);  
-            files=Object.assign(files,{
-                [fieldname]:dir.saveDir    
-            })
-            
-        }  
-        let article =new this.ctx.model.Article(Object.assign(files,parts.field));
+        let result = {};
+        try{
+            result = await this.ctx.service.upload.uploadImg();
+        }catch(error) {
+            throw error;
+        }
+        let article =new this.ctx.model.Article(result);
         await article.save();
         await this.success('/admin/article','增加文章成功')
     }
