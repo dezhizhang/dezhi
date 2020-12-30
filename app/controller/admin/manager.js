283fc7e3ced8723e3b7a01ce797f184ef971b3da
zhangdezhi <1541609448@qq.com>
2020-12-30 19:33:42 +0800

    管吾员代码重构

--- app/controller/admin/manager.js
+++ app/controller/admin/manager.js
@@ -16,32 +16,19 @@ class ManagerController extends BaseController {
     }
     //增加管理员
     async doAdd() {
-        let parts = this.ctx.multipart({ autoFields: true });
-        let files = {};               
-        let stream;
-        while ((stream = await parts()) != null) {
-            if (!stream.filename) {          
-                break;
-            }       
-            let fieldname = stream.fieldname;  //file表单的名字
-            //上传图片的目录
-            let dir=await this.service.tools.getUploadFile(stream.filename);
-            let target = dir.uploadDir;
-            let writeStream = fs.createWriteStream(target);
-            await pump(stream, writeStream);  
-            files=Object.assign(files,{
-                [fieldname]:dir.saveDir    
-            })
-            
-        }   
-        let result = await this.ctx.model.Admin.find({'email':parts.field.email});
-        if(result.length > 0) {
-            await this.error('/admin/manager/add','管理员以存在');
-            return
+        let result = {};
+        try{
+            result = await this.ctx.service.upload.uploadImg();
+        } catch(error) {
+            throw error;
         }
-
-        parts.field.password = await this.service.tools.md5(parts.field.password);
-        let admin =new this.ctx.model.Admin(Object.assign(files,parts.field));
+        let data = await this.ctx.model.Admin.find({'email':result.email});
+        if(data.length > 0) {
+            await this.error('/admin/manager/add','管理员以存在');
+            return;
+        }
+        let password = await this.ctx.service.tools.md5(result.password);
+        let admin =new this.ctx.model.Admin({password,...result});
         await admin.save();
         await this.success('/admin/manager','增加管理员成功');
     }
