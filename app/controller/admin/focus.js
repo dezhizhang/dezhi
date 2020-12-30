ce8af4377430841d2e7ee4c2d005cb879481cd39
zhangdezhi <1541609448@qq.com>
2020-12-30 17:04:59 +0800

    修改上传轮播图

--- app/controller/admin/focus.js
+++ app/controller/admin/focus.js
@@ -13,27 +13,14 @@ class FocusController extends BaseController {
     async add() {
         await this.ctx.render('/admin/focus/add');
     }
-
     async doAdd() {
-        let parts = this.ctx.multipart({ autoFields: true });
-        let files = {};               
-        let stream;
-        while ((stream = await parts()) != null) {
-            if (!stream.filename) {          
-                break;
-            }       
-            let fieldname = stream.fieldname;  //file表单的名字
-            //上传图片的目录
-            let dir=await this.service.tools.getUploadFile(stream.filename);
-            let target = dir.uploadDir;
-            let writeStream = fs.createWriteStream(target);
-            await pump(stream, writeStream);  
-            files=Object.assign(files,{
-                [fieldname]:dir.saveDir    
-            })
-            
-        }  
-        let admin =new this.ctx.model.Focus(Object.assign(files,parts.field));
+        let result = {};
+        try{
+            result = await this.service.upload.uploadImg();
+        } catch(error) {
+            throw error;
+        }
+        let admin =new this.ctx.model.Focus(result);
         await admin.save();
         await this.success('/admin/focus','增加轮播图成功');
     }
